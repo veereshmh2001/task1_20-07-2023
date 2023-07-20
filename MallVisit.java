c77564e5687d41c19cb40e3ba41d0afa9b3c03ea
veereshmh2001 <veereshmh.xworkz@gmail.com>
2023-07-20 19:43:21 +0530

    java prgms

---
+++ MallVisit.java
@@ -0,0 +1,9 @@
+class MallVisit{
+	
+	public static void main(String[] args){
+		
+		System.out.println("invoking main method in MallVisit");
+		
+		Mall mall=new Mall ("City Center Mall", "New York", 150, 300000.0, true, true, "Indoor Amusement Park");
+	}
+}
